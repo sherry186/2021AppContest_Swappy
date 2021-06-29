7740cdd75f35dc13229dcaa14247a71804e8fbd1
sylvey <80093140+sylvey@users.noreply.github.com>
2021-06-29 23:53:27 +0800

    Add files via upload

--- screens/Personal.js
+++ screens/Personal.js
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native';
 function Personal() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>presonal screen</Text>
+      <Text>personal screen</Text>
     </View>
   );
 }
