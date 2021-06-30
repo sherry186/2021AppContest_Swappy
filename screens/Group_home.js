e46caa85b8f2e135fc01b2d1613e97ba6963855c
sherry186 <b08705053@ntu.edu.tw>
2021-07-01 03:03:44 +0800

    added search bar

--- screens/Group_home.js
+++ screens/Group_home.js
@@ -1,29 +1,74 @@
 import * as React from 'react';
-import SearchBar from '../Components/SearchBar';
-import { View, Text, SafeAreaView, StyleSheet, TouchableOpacity } from "react-native";
+
+import { View, Text, SafeAreaView,  FlatList, StyleSheet, TouchableOpacity } from "react-native";
+import { SearchBar } from 'react-native-elements';
 import { useState } from "react";
 
+const GENERAL_DATA = [
+  {
+    id: '0',
+    title: 'Group 1',
+  },
+  {
+    id: '1',
+    title: 'Group 2',
+  },
+  {
+    id: '2',
+    title: 'Group 3',
+  },
+];
 
+const Item = ({ title }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
+
+
+const renderItem = ({ item }) => (
+  <Item title={item.title} />
+);
 
 export default class Group_HOME extends React.Component {
+
+  state = {
+    search: '',
+  };
 
   static navigationOptions = {
     title: 'Group_HOME',
   }
 
+  updateSearch = (search) => {
+    this.setState({ search });
+  };
+  
+
   render() {
+    const { search } = this.state;
     // const[grvalue, grsetValue] = useState('');
     const{ navigate } = this.props.navigation;
     //console.log(this.props.navigation);
 
     return(
-      <View style={styles.container}>
+      <SafeAreaView style={styles.container}>
+        <SearchBar
+        placeholder="Type Here..."
+        onChangeText={this.updateSearch}
+        value={search}
+      />
+      <FlatList
+        data={GENERAL_DATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
         <TouchableOpacity 
             style={styles.button}
             onPress={() => navigate('ADD')}>
             <Text style={styles.buttonText}>ADD</Text>
         </TouchableOpacity>
-      </View>
+      </SafeAreaView>
     )
   }
 
@@ -32,8 +77,8 @@ export default class Group_HOME extends React.Component {
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center'
+      // alignItems: 'center',
+      // justifyContent: 'center'
     },
     item: {
       backgroundColor: '#f9c2ff',
