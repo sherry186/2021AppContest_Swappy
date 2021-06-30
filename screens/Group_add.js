19653c68c568e88060157a81b7f7bba38b88fa24
sylvey <80093140+sylvey@users.noreply.github.com>
2021-06-30 22:02:40 +0800

    Add files via upload

--- screens/Group_add.js
+++ screens/Group_add.js
@@ -1,20 +1,30 @@
 import { styleSheets } from 'min-document';
-import React from 'react';
-import {Text, StyleSheet, View, Button, StatusBar} from 'react-native';
-
-
+import React, { useState } from 'react';
+import {
+    Text,
+    StyleSheet,
+    View, 
+    TextInput} from 'react-native';
 
 export default class Group_ADD extends React.Component {
 
   static navigationOptions = {
     title: 'Group_ADD',
   }
+  constructor(props) {
+    super(props);
+    this.state = { text: 'Useless Placeholder' };
+  }
+
 
   render() {
     const{ navigate } = this.props.navigation;
-
     return(
       <View style={styles.container}>
+        <TextInput
+            style={{height:40, borderColor: 'gray', borderWidth: 1}}
+            onChangeText={(text) => this.setState({text})}
+            value = {this.state.text}/>
         
         {/* <Button
             title = 'Go to home screen'
