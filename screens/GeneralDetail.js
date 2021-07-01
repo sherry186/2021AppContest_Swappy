24b4a51e21f2a85202b17fbfe528052ae8f053e0
sylvey <80093140+sylvey@users.noreply.github.com>
2021-07-01 12:34:32 +0800

    Add files via upload

---
+++ screens/GeneralDetail.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+import { View, Text, Button } from "react-native";
+
+function DetailsScreen({ route, navigation }) {
+    /* 2. Get the param */
+    const { itemID, title } = route.params;
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+        <Text>GeneralId: {itemID}</Text>
+        <Text>General Name: {title}</Text>
+        <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+        <Button title="Go back" onPress={() => navigation.goBack()} />
+      </View>
+    );
+  }
+
+export default DetailsScreen;
