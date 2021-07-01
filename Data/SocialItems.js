30660376e31bd5a8fff2a1527f57e18867f5763c
sylvey <80093140+sylvey@users.noreply.github.com>
2021-07-01 17:50:15 +0800

    Add files via upload

---
+++ Data/SocialItems.js
@@ -0,0 +1,17 @@
+export default [
+    {
+      id: '0',
+      person: 'Jane',
+      post: "This app is really good. Thank you WYL.",
+    },
+    {
+      id: '1',
+      person: 'Sherry',
+      post: "This app is quite bad. I don't like it", 
+    },
+    {
+      id: '2',
+      person: 'Sylvia',
+      post: "I found that some people really like this app, so I try to.....",
+    },
+];
