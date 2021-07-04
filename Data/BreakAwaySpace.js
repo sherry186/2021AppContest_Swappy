a9574e2ef0b0b85de2a6675ccd2cc358e17f24f8
sylvey <80093140+sylvey@users.noreply.github.com>
2021-07-04 23:18:23 +0800

    Add files via upload

---
+++ Data/BreakAwaySpace.js
@@ -0,0 +1,23 @@
+export default [
+    {
+      id: '0',
+      title: 'Bedroom',
+      complete: 0.12,
+    },
+    {
+        id: '1',
+        title: 'Living Room',
+        complete: 0.32,
+    },
+    {
+        id: '2',
+        title: 'Kitchen',
+        complete: 0.70,
+    },
+    {
+        id: '3',
+        title: 'Belcony',
+        complete: 0.42,
+    },
+
+];
