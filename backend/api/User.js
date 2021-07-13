8180d0d0cae8b60312364b6d202039515f84dbe8
sherry186 <b08705053@ntu.edu.tw>
2021-07-14 01:29:13 +0800

    updated backend

---
+++ backend/api/User.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/signup', (req, res) => {
+
+})
+
+router.post('/signin', (req, res) => {
+    
+})
+
+module.exports = router;
---
+++ backend/models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    username: String,
+    email: String,
+    password: String,
+    phoneNumber: String,
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
