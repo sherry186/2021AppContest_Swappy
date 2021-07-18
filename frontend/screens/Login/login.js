c4b3f0154a983de03442a38c22c5f188ce0c5690
sylvey <a.few.star@gmail.com>
2021-07-18 22:39:43 +0800

    login edit

--- frontend/screens/Login/login.js
+++ frontend/screens/Login/login.js
@@ -1,11 +1,25 @@
 import * as React from 'react';
-import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { View, 
+        Text, 
+        StyleSheet, 
+        TextInput, 
+        TouchableOpacity,
+        Image } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 import colors from '../../config/colors';
 
 export default class login extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { account: '', password: '' };
+    this.state = { 
+      account: '', 
+      password: '', 
+      accountImage: require("../../assets/login/account.png"),
+      passwordImage: require("../../assets/login/password.png"),
+      passwordEyeImage: require("../../assets/login/eye.png"),
+      secure: true,
+     };
+      
   }
 
   handlesubmit = () => {
@@ -16,17 +30,49 @@ export default class login extends React.Component {
     return (
       <View style={{ flex: 1 , backgroundColor: colors.function_100}}>
         <View style = {{flex: 1, borderTopStartRadius:20, borderTopRightRadius:20, backgroundColor: colors.mono_40, alignItems:'center', justifyContent:'center'}}>
-          <TextInput
-            style={styles.input}
-            placeholder='電子郵件'
-            onChangeText={(text) => { this.setState({ account: text }) }}
-            value={this.state.account} />
+          <View style = {styles.inputContainer}>
+              <Image
+                style={styles.image}
+                source={this.state.accountImage} />
+              <TextInput
+                style={styles.input}
+                placeholderTextColor = {colors.function_100}
+                placeholder='電子郵件'
+                onChangeText={(text) => { this.setState({ account: text }) }}
+                value={this.state.account} />
+          </View>
+          
+          <View style = {styles.inputContainer}>
+              <Image
+                style={styles.image}
+                source={this.state.passwordImage} />
+              <TextInput
+                style={styles.input}
+                placeholderTextColor = {colors.function_100}
+                placeholder='密碼'
+                secureTextEntry = {this.state.secure}
+                onChangeText={(text) => { this.setState({ password: text }) }}
+                value={this.state.password} />
 
-          <TextInput
-            style={styles.input}
-            placeholder='密碼'
-            onChangeText={(text) => { this.setState({ password: text }) }}
-            value={this.state.password} />
+              <TouchableOpacity
+                title='resetpassword'
+                onPress = {() => this.setState({secure: !this.state.secure})}
+                style ={{
+                  left: 110,
+                  top: 12,  
+                  width: 24, 
+                  height: 24,}}
+              >
+                <Image
+                  style ={{
+                    width: 24, 
+                    height: 24,
+                    tintColor: this.state.secure? colors.function_100 : colors.brown_40}} 
+                  source = {this.state.passwordEyeImage}/>
+              </TouchableOpacity>
+          </View>
+
+          
 
           <View style = {{left: 100, flexDirection: 'row'}}>
               <Text style = {{color: colors.mono_80}}>找不到密碼嗎？ </Text>
@@ -56,13 +102,33 @@ export default class login extends React.Component {
 
 
 const styles = StyleSheet.create({
-  input: {
+  
+  inputContainer:{
+    flexDirection: 'row',
     margin:10,
-    height: 40,
-    width: 300,
-    alignContent: 'center',
-    justifyContent: 'center',
-    borderWidth: 0
+    height: 48,
+    width: 356,
+    borderWidth: 1,
+    borderRadius:6,
+    borderColor: colors.function_100,
+    backgroundColor: 'transparent',
+
+  },
+  image:{
+    marginLeft: 13,
+    top: 12,  
+    width: 24, 
+    height: 24,  
+  },
+  input: {
+    top: 14,
+    left: 10,
+    height: 20.96,
+    width: 167.72,
+    borderWidth: 0,
+  },
+  eye:{
+     
   },
   submit: {
     borderRadius: 10,
