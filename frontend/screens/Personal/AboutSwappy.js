a004588b8c845563df53bdea1a85e3fe3c32f29f
sylvey <a.few.star@gmail.com>
2021-08-07 20:59:01 +0800

    About swappy

--- frontend/screens/Personal/AboutSwappy.js
+++ frontend/screens/Personal/AboutSwappy.js
@@ -49,11 +49,20 @@ const AboutSwappy = () => {
                     <Text style = {styles.content}>Swappy是由WYL.團隊開發出的交換平台，意在改善時下交換平台的痛點。Swappy針對易用性、互動性以及社群性為旨做開發，期望能為用戶帶來良好的交換體驗。</Text>
                     <Text></Text>
                     <Text style = {styles.title}>為什麼要用Swappy？</Text>
-                    <Text></Text>
-                    <Text style = {styles.content}>時下交換平台，常存在著換物不夠有效率、用戶猶豫換出物品時間過長、以及缺乏歸屬感的問題。Swappy針對此三點做分析，打造了其他換物平台所沒有的獨家功能：群組換物、斷捨離、社群發文機制，讓您在換物上有更良好的體驗</Text>
-                    <Text style = {styles.title}></Text>
-                    <Text></Text>
-                    <Text style = {styles.content}></Text>
+                    <View style = {styles.margin}></View>
+                    <Text style = {styles.content}>時下交換平台，常存在著換物不夠有效率、用戶猶豫換出物品時間過長、以及缺乏歸屬感的問題。Swappy針對此三點做分析，打造了其他換物平台所沒有的獨家功能：群組換物、斷捨離、社群發文機制，讓您在換物上有更良好的體驗。</Text>
+                    <View style = {styles.margin}></View>
+                    <Text style = {styles.title2}>群組換物功能</Text>
+                    <View style = {styles.margin}></View>
+                    <Text style = {styles.content}>Swappy提供獨家群組換物功能，讓用戶自創群組，在群組內配對物品，進行特定物品的交換。在提高效率的同時，也讓用戶能找到具有歸屬感的換物群體。</Text>
+                    <View style = {styles.margin}></View>
+                    <Text style = {styles.title2}>斷捨離挑戰</Text>
+                    <View style = {styles.margin}></View>
+                    <Text style = {styles.content}>Swappy提供自創的斷捨離挑戰，讓用戶可放上正在猶豫是否換出的物品，並設定時限，讓自己充分猶豫過後，決定是否換出物品。</Text>
+                    <View style = {styles.margin}></View>
+                    <Text style = {styles.title2}>社群</Text>
+                    <View style = {styles.margin}></View>
+                    <Text style = {styles.content}>相較其他換物平台，Swappy提供社群平台，讓用戶在換物過程中找到同好，建立換友群體中的歸屬感。</Text>
                 </ScrollView>
             </View>
             
@@ -68,12 +77,16 @@ const AboutSwappy = () => {
 export default AboutSwappy;
 
 const styles = StyleSheet.create({
+    margin:{
+        height:ScreenWidth*0.02,
+    },
     title:{
-        fontSize:ScreenWidth*0.04,
+        fontSize:ScreenWidth*0.042,
+        fontWeight:'bold',
         color: colors.function_100,
     },
     title2:{
-        fontSize:ScreenWidth*0.04,
+        fontSize:ScreenWidth*0.037,
         color: colors.function_80,
     },
     content:{
